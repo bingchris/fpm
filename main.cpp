ede5b60962b0afc293857519d51d6743ce6228b2
foobar <59284168+Tarvey@users.noreply.github.com>
2024-10-27 07:44:27 +0000

    legit 3 characters removed (show full name of action)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; ++i) {
         if (argv[i][0] == '-') {
-            action = argv[i][1];
+            action = argv[i];
             std::cout << "Action: " << action << std::endl;
         } else {
             std::cout << argv[i] << std::endl;
