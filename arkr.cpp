b56a4cc5d9813af48fa58c1ce87c78b715d6d4ec
Tarvey <59284168+Tarvey@users.noreply.github.com>
2024-12-17 19:58:53 +0100

    i forgot

--- arkr.cpp
+++ arkr.cpp
@@ -7,12 +7,10 @@ using namespace std;
 string get_mirlink() {
     array<char, 128> buffer;
     string result;
-    // Open a pipe to run the command
-    unique_ptr<FILE, decltype(&pclose)> pipe(popen("cat /etc/arkr/mirlink", "r"), pclose);
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen("cat /etc/arkr/mirlink | tr -d '\n'", "r"), pclose); /*http://example.com/packages/arch/*/
     if (!pipe) {
         throw runtime_error("popen() failed!");
     }
-    // Read the output of the command
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
         result += buffer.data();
     }
@@ -21,6 +19,7 @@ string get_mirlink() {
 
 
 int main(int argc, char *argv[]) {
+    string setmirlink = get_mirlink();
     int action; /*0 = help, 1 = install, 2 = uninstall*/
     if (argv[1]=="in"){
         action=1;
@@ -29,7 +28,7 @@ int main(int argc, char *argv[]) {
     }
     for (int i=2; i < argc; ++i) {
         if (action=1) {
-            get_mirlink();
+            unique_ptr<FILE, decltype(&pclose)> pipe(popen(("wget -q --show-progress " + setmirlink + argv[i]).c_str(), "r"), pclose);
         }
     }
 
