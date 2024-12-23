03cd42df36777a9fbe3491f710e875648a6799f3
Tarvey <59284168+Tarvey@users.noreply.github.com>
2024-12-23 14:44:50 +0100

    SUDO!@!!!!

--- arkr.cpp
+++ arkr.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdexcept>
+#include <fstream>
 #include <array>
 #include <memory>
 #include <sstream>
 #include <string>
-
+#include "json.hpp"
+#include <unistd.h>
+using json = nlohmann::json_abi_v3_11_3::json;
 using namespace std;
 
 string get_mirlink() {
@@ -23,10 +26,25 @@ string get_mirlink() {
 
 
 int main(int argc, char *argv[]) {
+    
     if (argc < 2) {
         cerr << "Usage: " << argv[0] << " <in|out> <package_name...>" << endl;
         return 1;
     }
+    json arkrjson;
+
+
+    json::array_t packlist;
+    ifstream arkrjsonfile("/etc/arkr.json");
+    if (!arkrjsonfile.is_open()) {
+        cerr << "Failed to open arkr.json! Does it exist?" << endl;
+    }
+    arkrjsonfile >> arkrjson;
+    arkrjsonfile.close();
+    if (arkrjson.contains("packages") && arkrjson["packages"].is_array()) {
+        packlist = arkrjson["packages"];
+    } else { cerr << "JSON does not contain an array named 'packages'!" << endl; return 1; }
+    
 
     string setmirlink = get_mirlink();
     string group, packagename;
@@ -34,8 +52,16 @@ int main(int argc, char *argv[]) {
 
     if (string(argv[1]) == "in") {
         action = 1;
+        if (geteuid() != 0) {
+        cerr << "This program must be run as root, please su, sudo or doas to run this program" << endl;
+        return 1;
+    }
     } else if (string(argv[1]) == "out") {
         action = 2;
+        if (geteuid() != 0) {
+        cerr << "This program must be run as root, please su, sudo or doas to run this program" << endl;
+        return 1;
+    }
     } else {
         cerr << "Invalid action: " << argv[1] << endl;
         return 1;
@@ -54,19 +80,17 @@ int main(int argc, char *argv[]) {
                 catcommand = "wget -q --show-progress " + setmirlink + group + "/" + packagename + "/";
             } else {
                 packagename = whatpack;
-                group = ""; // Or handle it as needed
+                group = "";
 
                 command = "wget -q " + setmirlink + packagename + "/adds";
                 catcommand = "wget -q --show-progress " + setmirlink + packagename + "/";
             }
 
-            // Run the wget command and wait for it to complete
             int ret = system(command.c_str());
             if (ret != 0) {
                 throw runtime_error("Command to get files failed!\n" + command);
             }
 
-            // Run the cat command to read the content of the adds file
             string catcmd = "cat adds";
             cout << "Binaries to download:" << endl;
             ret = system(catcmd.c_str());
